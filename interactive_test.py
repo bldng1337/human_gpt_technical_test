7928dee8a328f9bed231f54cd65a1f4ff5869a63
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2024-09-09 01:45:49 +0200

    Added multiple models and custom conversation

--- interactive_test.py
+++ interactive_test.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 import gradio as gr
 from llama_cpp import Llama
 
@@ -23,25 +23,121 @@ User: The capital of France
 Assistant: The capital of France is Paris
 User: <|endtile|>
 """.strip()
-llm = Llama.from_pretrained(
-	repo_id="ArliAI/Phi-3.5-mini-3.8B-ArliAI-RPMax-v1.1-GGUF",
-	filename="ArliAI-RPMax-3.8B-v1.1-fp16.gguf",
-)
-# llm = Llama.from_pretrained(
-# 	repo_id="bartowski/Phi-3.5-mini-instruct-GGUF",
-# 	filename="Phi-3.5-mini-instruct-IQ3_XS.gguf",
-#     n_gpu_layers=-1,
-# )
 
 def chatmsg(message, role):
     return {"role": role, "content": message}
 
+class Model:
+    def __init__(self):
+        pass
+    def __call__(self, msg:str, stop:List[str], max_tokens:int):
+        raise NotImplementedError
+    def conv(self, msgs:List[Dict[str, str]]):
+        raise NotImplementedError
+    def starttok(self, user:str):
+        raise NotImplementedError
+    def close(self):
+        pass
 
-def conv(msgs:List[Dict[str, str]]):
-    return "\n".join([f"<|{msg['role']}|>\n{msg['content']}<|end|>" for msg in msgs])
+class Phi35RPMax(Model):
+    def __init__(self):
+        self.llm = Llama.from_pretrained(
+            repo_id="ArliAI/Phi-3.5-mini-3.8B-ArliAI-RPMax-v1.1-GGUF",
+            filename="ArliAI-RPMax-3.8B-v1.1-fp16.gguf",
+        )
+        
+    def __call__(self, msg:str, stop:List[str], max_tokens:int):
+        return self.llm(msg, stop=stop, max_tokens=max_tokens)
+    
+    def conv(self,msgs:List[Dict[str, str]]):
+        return "\n".join([f"<|{msg['role']}|>\n{msg['content']}<|end|>" for msg in msgs])
+    def starttok(self,user:str):
+        return f"<|{user}|>\n"
+    def close(self):
+        self.llm.close()
+Phi35RPMax.modelname="Phi35RPMax-fp16"
+class Phi35(Model):
+    def __init__(self):
+        self.llm = Llama.from_pretrained(
+            repo_id="bartowski/Phi-3.5-mini-instruct-GGUF",
+            filename="Phi-3.5-mini-instruct-IQ3_XS.gguf",
+        )
+    def __call__(self, msg:str, stop:List[str], max_tokens:int):
+        return self.llm(msg, stop=stop, max_tokens=max_tokens)
+    
+    def conv(self,msgs:List[Dict[str, str]]):
+        return "\n".join([f"<|{msg['role']}|>\n{msg['content']}<|end|>" for msg in msgs])
+    
+    def starttok(self,user:str):
+        return f"<|{user}|>\n"
+    def close(self):
+        self.llm.close()
+Phi35.modelname="Phi35-IQ3_XS"
+
+# TODO: Gemma2 needs license maybe try it in the future but dont think it is worth it
+# class Gemma2(Model):
+#     def __init__(self):
+#         self.llm = Llama.from_pretrained(
+#             repo_id="google/gemma-2-2b-it-GGUF",
+#             filename="2b_it_v2.gguf",
+#         )
+#     def __call__(self, msg:str, stop:List[str], max_tokens:int):
+#         return self.llm(msg, stop=stop, max_tokens=max_tokens)
+    
+#     def conv(self,msgs:List[Dict[str, str]]):#https://ai.google.dev/gemma/docs/formatting?hl=de
+#         return "\n".join([f"<|{msg['role']}|>\n{msg['content']}<|end|>" for msg in msgs])
+#     def formatmessage(self,msg:str, role:str):#https://ai.google.dev/gemma/docs/formatting?hl=de
+#         if(role=="system"):
+#             # Gemma2 does not support system messages / isnt trained for them
+#             # TODO: Make them Assistant messages and test if this improves the results
+#             return ""
+#         if role=="assistant":
+#             role="model"
+#         return f"<start_of_turn>{role}\n{msg}<end_of_turn>"
+#     def starttok(self,user:str):
+#         return f"<start_of_turn>{user}\n"
+#     def close(self):
+#         self.llm.close()
+# Gemma2.modelname="Gemma2-2b-it-GGUF"
+
+class Llama31uncensored(Model):
+    def __init__(self):
+        self.llm = Llama.from_pretrained(
+            repo_id="Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2-GGUF",
+            filename="Llama-3.1-8B-Lexi-Uncensored_V2_F16.gguf",
+        )
+    def __call__(self, msg:str, stop:List[str], max_tokens:int):
+        return self.llm(msg, stop=stop, max_tokens=max_tokens)
+    
+    def conv(self,msgs:List[Dict[str, str]]):
+        return "\n".join([f"<|begin_of_text|><|start_header_id|>{msg['role']}<|end_header_id|>\n\n{msg['content']}<|eot_id|>" for msg in msgs])
+    def starttok(self,user:str):
+        return f"<|begin_of_text|><|start_header_id|>{user}<|end_header_id|>\n\n"
+    def close(self):
+        self.llm.close()
+Llama31uncensored.modelname="Llama31-uncensored-fp16"
+
+class Llama31(Model):
+    def __init__(self):
+        self.llm = Llama.from_pretrained(
+            repo_id="lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF",
+            filename="Meta-Llama-3.1-8B-Instruct-IQ4_XS.gguf",
+        )
+    def __call__(self, msg:str, stop:List[str], max_tokens:int):
+        return self.llm(msg, stop=stop, max_tokens=max_tokens)
+    
+    def conv(self,msgs:List[Dict[str, str]]):
+        return "\n".join([f"<|begin_of_text|><|start_header_id|>{msg['role']}<|end_header_id|>\n\n{msg['content']}<|eot_id|>" for msg in msgs])
+    def starttok(self,user:str):
+        return f"<|begin_of_text|><|start_header_id|>{user}<|end_header_id|>"
+    def close(self):
+        self.llm.close()
+Llama31.modelname="Llama31-IQ4_XS"
 
 
-conversations=[
+models=[Phi35RPMax,Phi35,Llama31uncensored,Llama31]
+currmodel=Phi35()
+conversations:List[Dict[str, Any]]=[
     #More Trivia Style Question
     {"name":"Country","content":[{"role":"user","content":"What is the capital?"}]},
     {"name":"Simple Math","content":[{"role":"user","content":"What is 3*4?"}]},
@@ -94,21 +190,53 @@ conversations=[
 ]
 
 with gr.Blocks() as demo:
+    with gr.Accordion("Info"):
+        gr.Markdown(f"""
+                    # HumanGPT Game Test
+                    ## Disclaimer
+                    This is a test of feasibility and to evaluate different models, prompts, and types of conversations.
+                    The current conversations don't represent the type of interactions the final game would have, but rather showcase various possible scenarios for playtesting and assessing model behavior.
+                    This playground will also be used to test fine-tuned models in the future.
+                    ## How to Use
+                    - Use the chat window to test the model interactively.
+                    - If the model responds with "End of conversation," it means the interaction is over.
+                    - Change the conversation by selecting a different option from the choice box.
+                    - Change the model by selecting a different option from the model choice box.
+                    - To modify the system prompt, edit the text in the system prompt text box.
+                    - If you choose Custom in the conversation choice box, you can enter a custom conversation in the text box under the Custom Conversation accordion.
+                    """)
     chatbot = gr.Chatbot([chatmsg("What is the capital?","user")],type="messages",show_copy_all_button=True)
     msg = gr.Textbox()
     submit = gr.Button("Submit")
-    with gr.Accordion("See Details"):
-        choicebox = gr.Radio(choices=[conversation["name"] for conversation in conversations], value="Country", label="Conversations")
-        sysprompt=gr.Textbox(value=syspropmt, label="System Prompt")
-        def update_choicebox(choice):
+    with gr.Accordion("Config"):
+        convchoicebox = gr.Radio(choices=[conversation["name"] for conversation in conversations]+["Custom"], value="Country", label="Conversations")
+        with gr.Accordion("Custom Conversation",open=False):
+            custom_conv=gr.Textbox(value="", label="Conversation")
+            def update_custom_conv(custom_conv,convchoicebox,chatbot,msg):
+                if(convchoicebox=="Custom"):
+                    return "", [chatmsg(custom_conv,"user")]
+                return msg,chatbot
+            custom_conv.change(update_custom_conv, [custom_conv,convchoicebox,chatbot,msg], [msg,chatbot])
+        def update_choicebox(choice,custom_conv):
+            if(choice=="Custom"):
+                return "", [chatmsg(custom_conv,"user")]
             return "", next(conversation for conversation in conversations if conversation["name"] == choice)["content"]
-        choicebox.change(update_choicebox, [choicebox], [msg,chatbot])
+        sysprompt=gr.Textbox(value=syspropmt, label="System Prompt")
+        convchoicebox.change(update_choicebox, [convchoicebox,custom_conv], [msg,chatbot])
+        modelchoicebox = gr.Radio(choices=[model.modelname for model in models], value=currmodel.modelname, label="Model")
+        def update_modelchoicebox(choice):
+            global currmodel
+            currmodel.close()
+            currmodel=next(model for model in models if model.modelname == choice)()
+            return "", []
+        modelchoicebox.change(update_modelchoicebox, [modelchoicebox], [msg,chatbot])
 
     def respond(message:str, chat_history:List[Dict[str, str]],syspropmt:str):
+        global currmodel
         if "End of conversation." in [i["content"] for i in chat_history]:
             return "", chat_history
         chat_history.append(chatmsg(message,"assistant"))
-        ret=llm(conv(chatmsg(syspropmt,"system"))+conv(chat_history)+"<|user|>\n", stop=[".","\n \n","?\n",".\n","tile|>"],max_tokens=100)#Only stop at tile|> to see if the conv got terminated
+        ret=currmodel(currmodel.conv([chatmsg(syspropmt,"system")])+currmodel.conv(chat_history)+"<|user|>\n", stop=[".","\n \n","?\n",".\n","tile|>"],max_tokens=100)
         comp=ret["choices"][0]["text"]
         print(repr(comp))
         if("<|end" in comp):
@@ -117,7 +245,6 @@ with gr.Blocks() as demo:
         else:
             chat_history.append(chatmsg(comp,"user"))
         return "", chat_history
-
     submit.click(respond, [msg, chatbot,sysprompt], [msg, chatbot])
     msg.submit(respond, [msg, chatbot,sysprompt], [msg, chatbot])
 
